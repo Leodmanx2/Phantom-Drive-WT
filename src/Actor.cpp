37b2f282e442060645df91c05d76af0c4c8c6fd1
Leodmanx2 <chris.macleod@carleton.ca>
2015-06-12 18:59:28 -0400

    Fixes to how GLM was being used

--- src/Actor.cpp
+++ src/Actor.cpp
@@ -28,9 +28,10 @@ void Actor::rotate(float roll, float pitch, float yaw) {
 	canonicalUp = glm::vec3(0.0f, 1.0f, 0.0f);
 	canonicalLeft = glm::vec3(-1.0f, 0.0f, 0.0f);
 
-	m_forward = m_orientation.toMat4() * canonicalForward;
-	m_up = m_orientation.toMat4() * canonicalUp;
-	m_left = m_orientation.toMat4() * canonicalLeft;
+	glm::mat4 orientationMatrix = glm::mat4_cast(m_orientation);
+	m_forward = orientationMatrix * canonicalForward;
+	m_up = orientationMatrix * canonicalUp;
+	m_left = orientationMatrix * canonicalLeft;
 }
 
 void Actor::update() {
@@ -38,10 +39,10 @@ void Actor::update() {
 }
 
 void Actor::draw(float* viewMatrix, float* projectionMatrix) {
-	glm::mat4 rotationMatrix = m_orientation.toMat4();
+	glm::mat4 rotationMatrix = glm::mat4_cast(m_orientation);
 	glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), m_position);
 	
-	glm::mat4 modelMatrix = roationMatrix * translationMatrix;
+	glm::mat4 modelMatrix = translationMatrix * roationMatrix;
 	
 	m_renderModel->draw(glm::value_ptr(modelMatrix), 
 	                    viewMatrix, 
