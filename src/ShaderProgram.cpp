2e7ed3d2547d9d02ab040797e4d8bd34e2c4cf1a
Leo <chris@chris-macleod.ca>
2019-09-28 12:38:12 -0400

    Retain source strings
    It looks like globjects::Shader also tracks these for changes. I don't want to keep whole source strings im memory, so be sure to revisit this later.

--- src/ShaderProgram.cpp
+++ src/ShaderProgram.cpp
@@ -9,13 +9,14 @@ using namespace gl;
 using namespace globjects;
 
 ShaderProgram::ShaderProgram(const string& name) : m_program() {
-	string                         vertexSource = readFile(name + ".vert.glsl"s);
-	unique_ptr<StaticStringSource> vss = Shader::sourceFromString(vertexSource);
-	m_vertexShader = make_unique<Shader>(GL_VERTEX_SHADER, vss.get());
+	string vertexSource = readFile(name + ".vert.glsl"s);
+	m_vertexSource      = Shader::sourceFromString(vertexSource);
+	m_vertexShader = make_unique<Shader>(GL_VERTEX_SHADER, m_vertexSource.get());
 
-	string fragmentSource              = readFile(name + ".frag.glsl"s);
-	unique_ptr<StaticStringSource> fss = Shader::sourceFromString(fragmentSource);
-	m_fragmentShader = make_unique<Shader>(GL_FRAGMENT_SHADER, fss.get());
+	string fragmentSource = readFile(name + ".frag.glsl"s);
+	m_fragmentSource      = Shader::sourceFromString(fragmentSource);
+	m_fragmentShader =
+	  make_unique<Shader>(GL_FRAGMENT_SHADER, m_fragmentSource.get());
 
 	m_program->attach(m_vertexShader.get());
 	m_program->attach(m_fragmentShader.get());
--- src/ShaderProgram.hpp
+++ src/ShaderProgram.hpp
@@ -4,10 +4,15 @@
 #include <globjects/globjects.h>
 #include <memory>
 
+// TODO: Eliminate shader sources from memory
+//       They are only being retained because globjects::Shader tracks them
+
 class ShaderProgram {
-	std::unique_ptr<globjects::Shader>  m_vertexShader;
-	std::unique_ptr<globjects::Shader>  m_fragmentShader;
-	std::unique_ptr<globjects::Program> m_program;
+	std::unique_ptr<globjects::AbstractStringSource> m_vertexSource;
+	std::unique_ptr<globjects::AbstractStringSource> m_fragmentSource;
+	std::unique_ptr<globjects::Shader>               m_vertexShader;
+	std::unique_ptr<globjects::Shader>               m_fragmentShader;
+	std::unique_ptr<globjects::Program>              m_program;
 
 	public:
 	explicit ShaderProgram(const std::string& name);
