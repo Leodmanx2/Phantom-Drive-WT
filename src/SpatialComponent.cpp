914cde0836eebff9f8b7ef076859499fb722263f
Leo <chris@chris-macleod.ca>
2019-12-28 19:39:37 -0500

    Remove namepsace clutter

--- src/SpatialComponent.cpp
+++ src/SpatialComponent.cpp
@@ -3,31 +3,29 @@
 #include <plog/Log.h>
 
 using namespace plog;
+using namespace glm;
 
-const glm::vec4 SpatialComponent::canonicalForward =
-  glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
-const glm::vec4 SpatialComponent::canonicalUp =
-  glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
-const glm::vec4 SpatialComponent::canonicalLeft =
-  glm::vec4(-1.0f, 0.0f, 0.0f, 0.0f);
+const vec4 SpatialComponent::canonicalForward = vec4(0.0f, 0.0f, -1.0f, 0.0f);
+const vec4 SpatialComponent::canonicalUp      = vec4(0.0f, 1.0f, 0.0f, 0.0f);
+const vec4 SpatialComponent::canonicalLeft    = vec4(-1.0f, 0.0f, 0.0f, 0.0f);
 
 SpatialComponent::SpatialComponent()
-  : m_orientation(glm::quat(0.0f, 0.0f, 1.0f, 0.0f))
-  , m_position(glm::vec4(0.0f, 0.0f, 0.0f, 0.0f)) {}
+  : m_orientation(quat(0.0f, 0.0f, 1.0f, 0.0f))
+  , m_position(vec4(0.0f, 0.0f, 0.0f, 0.0f)) {}
 
 // Moving the given amount along the axes defined by m_orientation.
 void SpatialComponent::translate(float longitude,
                                  float latitude,
                                  float altitude) {
-	const glm::mat4 orientationMatrix = glm::mat4_cast(m_orientation);
+	const mat4 orientationMatrix = mat4_cast(m_orientation);
 
-	const glm::vec4 forward = orientationMatrix * canonicalForward;
-	const glm::vec4 up      = orientationMatrix * canonicalUp;
-	const glm::vec4 left    = orientationMatrix * canonicalLeft;
+	const vec4 forward = orientationMatrix * canonicalForward;
+	const vec4 up      = orientationMatrix * canonicalUp;
+	const vec4 left    = orientationMatrix * canonicalLeft;
 
-	const glm::vec4 longVec = forward * longitude;
-	const glm::vec4 latVec  = left * latitude;
-	const glm::vec4 altVec  = up * altitude;
+	const vec4 longVec = forward * longitude;
+	const vec4 latVec  = left * latitude;
+	const vec4 altVec  = up * altitude;
 
 	m_position += longVec + latVec + altVec;
 }
@@ -36,32 +34,31 @@ void SpatialComponent::translate(float longitude,
 // degrees. Each call to rotate() updates m_orientation, so rotations do not
 // compose additively.
 void SpatialComponent::rotate(float roll, float pitch, float yaw) {
-	const float     rollRad  = glm::radians(roll);
-	const float     pitchRad = glm::radians(pitch);
-	const float     yawRad   = glm::radians(yaw);
-	const glm::quat rotationQuat(glm::vec3(pitchRad, yawRad, rollRad));
+	const float rollRad  = radians(roll);
+	const float pitchRad = radians(pitch);
+	const float yawRad   = radians(yaw);
+	const quat  rotationQuat(vec3(pitchRad, yawRad, rollRad));
 	m_orientation *= rotationQuat;
 }
 
 // Directly sets the orientation, overriding any previous values.
 void SpatialComponent::setOrientation(float rad, float x, float y, float z) {
-	m_orientation = glm::quat(rad, x, y, z);
+	m_orientation = quat(rad, x, y, z);
 }
 
 // Directly sets the position, overriding any previous values.
 void SpatialComponent::setPosition(float x, float y, float z) {
-	m_position = glm::vec4(x, y, z, 0);
+	m_position = vec4(x, y, z, 0);
 }
 
-const glm::vec4& SpatialComponent::position() const { return m_position; }
+const vec4& SpatialComponent::position() const { return m_position; }
 
-const glm::quat& SpatialComponent::orientation() const { return m_orientation; }
+const quat& SpatialComponent::orientation() const { return m_orientation; }
 
 // Computes a 4x4 matrix representing the complete world-space transformation
-const glm::mat4 SpatialComponent::matrix() const {
-	const glm::mat4 rotationMatrix = glm::mat4_cast(m_orientation);
-	const glm::mat4 translationMatrix =
-	  glm::translate(glm::mat4(), glm::vec3(m_position));
+const mat4 SpatialComponent::matrix() const {
+	const mat4 rotationMatrix    = mat4_cast(m_orientation);
+	const mat4 translationMatrix = glm::translate(mat4(), vec3(m_position));
 
 	return translationMatrix * rotationMatrix;
 }
