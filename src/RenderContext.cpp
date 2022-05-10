1327632edb05c170093bad8a7d8c789f53cf5134
Leo <chris@chris-macleod.ca>
2022-05-10 16:14:59 -0400

    Move templated function definition to header

--- src/RenderContext.cpp
+++ src/RenderContext.cpp
@@ -1,6 +1,6 @@
 #include "RenderContext.hpp"
 
-#include <glbinding/gl/gl.h>
+#include "ShaderProgram.hpp"
 
 using namespace std;
 using namespace gl;
@@ -22,58 +22,6 @@ namespace PD {
 		globjects::Program& vertexShader = *m_ambient_pipeline->vertex_shader();
 		vertexShader.setUniform("ambience", ambience);
 		vao.drawElements(GL_TRIANGLES, elements, GL_UNSIGNED_INT);
-	}
-
-	template <input_iterator Iterator>
-	void RenderContext::highlight_pass(Iterator           begin,
-	                                   Iterator           end,
-	                                   const VertexArray& vao,
-	                                   const int          elements) {
-		glEnable(GL_BLEND);
-		glBlendEquation(GL_FUNC_ADD);
-		glBlendFunc(GL_ONE, GL_ONE);
-		m_highlight_pipeline->raw()->use();
-		globjects::Program& fragment_shader =
-		  *m_highlight_pipeline->fragment_shader();
-		while(begin != end) {
-			auto light = *begin;
-			fragment_shader.setUniform("light.position", light.position);
-			fragment_shader.setUniform("light.direction", light.direction);
-			fragment_shader.setUniform("light.color", light.color);
-			fragment_shader.setUniform("light.intensity", light.intensity);
-			fragment_shader.setUniform("light.angle", light.angle);
-			fragment_shader.setUniform("light.radius", light.radius);
-
-			vao.drawElements(GL_TRIANGLES, elements, GL_UNSIGNED_INT);
-			++begin;
-		}
-		glDisable(GL_BLEND);
-	}
-
-	template <std::input_iterator Iterator>
-	void RenderContext::draw(const textures       textures,
-	                         const Geometry&      geometry,
-	                         const int            id,
-	                         const mvp_transforms transforms,
-	                         const glm::vec3      eye,
-	                         const float          ambience,
-	                         Iterator             lights_begin,
-	                         Iterator             lights_end) {
-		geometry.vao().bind();
-		textures.albedo->bindActive(FragmentShaderProgram::ALBEDO_TEXTURE_UNIT);
-		// TODO: Bind other texture units
-
-		m_ambient_pipeline->vertex_shader().transforms(
-		  transforms.model, transforms.view, transforms.projection);
-		m_ambient_pipeline->fragment_shader().camera(transforms.view, eye);
-		ambient_pass(geometry.vao(), geometry.elements(), ambience);
-
-		m_highlight_pipeline->vertex_shader().transforms(
-		  transforms.model, transforms.view, transforms.projection);
-		m_highlight_pipeline->fragment_shader().camera(transforms.view, eye);
-		highlight_pass(
-		  lights_begin(), lights_end(), geometry.vao(), geometry.elements());
-		// TODO: return ID map
 	}
 
 } // namespace PD
--- src/RenderContext.hpp
+++ src/RenderContext.hpp
@@ -7,6 +7,7 @@
 #include "Renderer.hpp"
 #include "ShaderPipeline.hpp"
 
+#include <glbinding/gl/gl.h>
 #include <globjects/ProgramPipeline.h>
 #include <globjects\VertexArray.h>
 #include <iterator>
@@ -26,11 +27,32 @@ namespace PD {
 		                  const int                     elements,
 		                  const float                   ambience);
 
-		template <std::input_iterator Iterator>
-		void highlight_pass(Iterator                      lights_begin,
-		                    Iterator                      lights_end,
+		//template <std::input_iterator Iterator>
+		template <typename Iterator>
+		void highlight_pass(Iterator                      begin,
+		                    Iterator                      end,
 		                    const globjects::VertexArray& vao,
-		                    const int                     elements);
+		                    const int                     elements) {
+			glEnable(gl::GL_BLEND);
+			glBlendEquation(gl::GL_FUNC_ADD);
+			glBlendFunc(gl::GL_ONE, gl::GL_ONE);
+			m_highlight_pipeline->raw()->use();
+			globjects::Program& fragment_shader =
+			  *m_highlight_pipeline->fragment_shader();
+			while(begin != end) {
+				auto light = *begin;
+				fragment_shader.setUniform("light.position", light.position);
+				fragment_shader.setUniform("light.direction", light.direction);
+				fragment_shader.setUniform("light.color", light.color);
+				fragment_shader.setUniform("light.intensity", light.intensity);
+				fragment_shader.setUniform("light.angle", light.angle);
+				fragment_shader.setUniform("light.radius", light.radius);
+
+				vao.drawElements(gl::GL_TRIANGLES, elements, gl::GL_UNSIGNED_INT);
+				++begin;
+			}
+			glDisable(gl::GL_BLEND);
+		}
 
 		public:
 		RenderContext(framebuffer_ptr frame_buffer,
@@ -45,15 +67,32 @@ namespace PD {
 			return *m_highlight_pipeline;
 		}
 
-		template <std::input_iterator Iterator>
-		void draw(const textures       albedo,
+		//template <std::input_iterator Iterator>
+		template <typename Iterator>
+		void draw(const textures       textures,
 		          const Geometry&      geometry,
 		          const int            id,
 		          const mvp_transforms transforms,
 		          const glm::vec3      eye,
 		          const float          ambience,
 		          Iterator             lights_begin,
-		          Iterator             lights_end);
+		          Iterator             lights_end) {
+			geometry.vao().bind();
+			textures.albedo->bindActive(FragmentShaderProgram::ALBEDO_TEXTURE_UNIT);
+			// TODO: Bind other texture units
+
+			m_ambient_pipeline->vertex_shader().transforms(
+			  transforms.model, transforms.view, transforms.projection);
+			m_ambient_pipeline->fragment_shader().camera(transforms.view, eye);
+			ambient_pass(geometry.vao(), geometry.elements(), ambience);
+
+			m_highlight_pipeline->vertex_shader().transforms(
+			  transforms.model, transforms.view, transforms.projection);
+			m_highlight_pipeline->fragment_shader().camera(transforms.view, eye);
+			highlight_pass(
+			  lights_begin, lights_end, geometry.vao(), geometry.elements());
+			// TODO: return ID map
+		}
 	};
 
 } // namespace PD
